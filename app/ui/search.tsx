86092bb6ac8c0a61d36d1f33434990b44e200a3c
Leo <leomagnifico06@gmail.com>
2026-05-26 12:23:21 +0200

    third commit

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,29 +1,29 @@
 'use client';
-
+ 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-import { useDebouncedCallback } from 'use-debounce';
-
+import { useSearchParams, usePathname, useRouter } from 'next/navigation'
+import { useDebouncedCallback } from 'use-debounce'
+ 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const { replace } = useRouter()
 
-  function handleSearch(term: string) {
-    
-  const handleSearch = useDebouncedCallback((term) =>{
+  const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
 
-    const params = new URLSearchParams(searchParams)
-    if(term){
-      params.set('query', term)
-    }else{
-      params.delete('query')
-    }
-    replace(`${pathname}?${params.toString()}`)
-  }, 300)
+    const params = new URLSearchParams(searchParams);
+    params.set('page','1')
+    if (term) {
+        params.set('query', term);
+      } else {
+        params.delete('query');
+      }
+      replace(`${pathname}?${params.toString()}`);
+    }, 300);
+ 
   return (
-    <div className="relative flex flex-1 flex-shrink-0">
+    <div className="relative flex flex-1 shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
@@ -37,6 +37,5 @@ export default function Search({ placeholder }: { placeholder: string }) {
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
-    );
-  }
+  );
 }
